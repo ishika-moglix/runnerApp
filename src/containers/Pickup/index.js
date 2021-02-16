10d7769f3c491af5490340f5d366c4b559d55ca7
antimagupta009 <antima.gupta@moglix.com>
2021-02-16 12:47:58 +0530

    issue

--- src/containers/Pickup/index.js
+++ src/containers/Pickup/index.js
@@ -1,6 +1,6 @@
-import {Container, Icon} from 'native-base';
+import { Container, Icon } from 'native-base';
 import React from 'react';
-import {Image, TouchableOpacity, View, FlatList} from 'react-native';
+import { Image, TouchableOpacity, View, FlatList } from 'react-native';
 
 import CompanyCard from '../../components/Cards/CompayCard';
 import Header from '../../components/Header';
@@ -25,18 +25,18 @@ export default PickupScreen = (props) => {
     props.navigation.openDrawer();
   };
 
-  const renderCards = ({item, index}) => {
+  const renderCards = ({ item, index }) => {
     return <CompanyCard navigation={props.navigation} item={item} />;
   };
 
   return (
-    <Container style={{backgroundColor: '#F2F2F2'}}>
+    <Container style={{ backgroundColor: '#F2F2F2' }}>
       <Header
         headertext={'Pick Up'}
         leftComponent={() => (
           <TouchableOpacity onPress={openDrawer}>
             <Image
-              style={{width: 20, height: 20}}
+              style={{ width: 20, height: 20 }}
               source={require('../../assets/menu.png')}
             />
           </TouchableOpacity>
@@ -45,7 +45,7 @@ export default PickupScreen = (props) => {
           <TouchableOpacity>
             <Icon
               name={'magnify'}
-              style={{color: '#fff'}}
+              style={{ color: '#fff' }}
               type={'MaterialCommunityIcons'}
             />
           </TouchableOpacity>
